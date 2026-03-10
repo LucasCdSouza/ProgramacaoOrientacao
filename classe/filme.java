c1bb660f4779795ee798e214e87c04fd97ff001f
Lucas Camargo de Souza <lucascamargodsz@gmail.com>
2026-03-10 19:05:18 -0300

    poo

---
+++ classe/filme.java
@@ -0,0 +1,9 @@
+package pkg;
+
+public class filme{
+    public String titulo;
+    public String diretor;
+    public int anolancamento;
+    public String genero;
+}
+
---
+++ classe/principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main (String{} args){
+        int n;
+        Filme f = new Filme();
+        System.out.println("Objeto f instanciado!")
+    }
+
+}
